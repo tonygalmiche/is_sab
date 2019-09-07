2fe170ee536ae2d6fea256e7c9309da1092c76f4
Tony Galmiche <tony.galmiche@infosaone.com>
2019-09-07 09:30:06 +0200

    modif

--- models/is_materiel.py
+++ models/is_materiel.py
@@ -20,23 +20,25 @@ class IsMateriel(models.Model):
     @api.depends('pret_ids')
     def _compute(self):
         for obj in self:
-            date_debut = False
-            date_fin   = False
+            date_debut  = False
+            date_fin    = False
+            date_retour = False
             for p in obj.pret_ids:
-                date_debut = p.date_debut
-                date_fin   = p.date_fin
-            #if not date_fin and date_debut:
-            #    date_fin = date_debut + timedelta(days=365)
-            obj.date_debut = date_debut
-            obj.date_fin   = date_fin 
-
+                date_debut  = p.date_debut
+                date_fin    = p.date_fin
+                date_retour = p.date_retour
+            if not date_fin and date_debut:
+                date_fin = date_debut + timedelta(days=14)
+            obj.date_debut  = date_debut
+            obj.date_fin    = date_fin
+            obj.date_retour = date_retour
 
     name             = fields.Char("Nom", required=True, index=True)
     type_materiel_id = fields.Many2one('is.type.materiel', 'Type de matériel', required=True,index=True)
     pret_ids         = fields.One2many('is.materiel.pret', 'materiel_id', u'Prêts')
     date_debut       = fields.Date("Date de début", compute='_compute', readonly=True, store=True)
     date_fin         = fields.Date("Date de fin"  , compute='_compute', readonly=True, store=True)
-
+    date_retour      = fields.Date("Date de retour", compute='_compute', readonly=True, store=True)
 
 
 class IsMaterielPret(models.Model):
@@ -49,6 +51,7 @@ class IsMaterielPret(models.Model):
     membre_id   = fields.Many2one('res.partner', 'Membre'   , required=True)
     date_debut  = fields.Date("Date de début", required=True, index=True)
     date_fin    = fields.Date("Date de fin")
+    date_retour = fields.Date("Date de retour")
     note        = fields.Text("Note")
 
 
