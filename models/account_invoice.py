b734d5000d5d4526b0ed4680f1956a1f93f7cbd1
Tony Galmiche <tony.galmiche@infosaone.com>
2020-12-18 10:19:06 +0100

    bug

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -7,15 +7,13 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     @api.onchange('product_id')
-    def _onchange_product_id(self):
-        res = super(AccountInvoiceLine, self)._onchange_product_id()
+    def _onchange_product_id_account_analytic_id(self):
         if self.product_id.is_account_analytic_id and not self.account_analytic_id:
             self.account_analytic_id = self.product_id.is_account_analytic_id.id
-        return res
 
 
     @api.model
     def create(self, vals):
         res = super(AccountInvoiceLine, self).create(vals)
-        res._onchange_product_id()
+        res._onchange_product_id_account_analytic_id()
         return res
